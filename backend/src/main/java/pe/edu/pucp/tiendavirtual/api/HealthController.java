f4d22145a3e86564130757ba2f9d122c5ff10e9f
Juan Luis Saavedra Bazan <jlsaavedrab@gmail.com>
2026-05-29 20:00:39 -0500

    Nuevo balanceador de carga

---
+++ backend/src/main/java/pe/edu/pucp/tiendavirtual/api/HealthController.java
@@ -0,0 +1,11 @@
+package pe.edu.pucp.tiendavirtual.api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
