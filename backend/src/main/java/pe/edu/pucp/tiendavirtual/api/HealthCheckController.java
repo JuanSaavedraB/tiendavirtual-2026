15bfb333870e96b5385a7a366d74b21d3d0986fc
Juan Luis Saavedra Bazan <jlsaavedrab@gmail.com>
2026-05-29 20:24:01 -0500

    rename

--- backend/src/main/java/pe/edu/pucp/tiendavirtual/api/HealthCheckController.java
+++ backend/src/main/java/pe/edu/pucp/tiendavirtual/api/HealthCheckController.java
@@ -3,7 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
+public class HealthCheckController {
     @GetMapping("/")
     public String health() {
         return "OK";
